14f5a02280fcd44b9f2f26718d1fc1ff7d7e7a17
gui machiavelli <hey@guimachiavelli.com>
2014-12-19 15:55:49 +0000

    discarding verses that do not refer the same personal pronoun

--- src/generate-poem.rb
+++ src/generate-poem.rb
@@ -1,22 +1,72 @@
+PRONOUNS = ['I', 'you', 'thou', 'ye', 'he', 'she', 'it', 'they', 'we']
+
 def generate_poem
     data = get_verse_data()
     verses = rand 3..10
+    poem = []
 
-    test = pick_verses data, verses
+    poem = pick_verses data, verses, poem
 
-    puts test
+    puts poem
+
 end
 
 
-def pick_verses(data, verses)
-    poem = []
-    while verses > 0
-        chosen_base = data[rand(data.length)]
-        poem.push chosen_base[rand(chosen_base.length)]
+def pick_verses(data, verses, poem)
+    if verses <= 0 then return poem end
+
+    chosen_base = data[rand(data.length)]
+    chosen_verse = chosen_base[rand(chosen_base.length)]
+
+    if good_enough? chosen_verse, poem
+        poem.push chosen_verse
         verses -= 1
     end
-    poem
+
+    pick_verses data, verses, poem
 end
+
+def good_enough?(chosen_verse, poem)
+    last_verse = poem[-1]
+
+    if last_verse == nil
+        return true
+    end
+
+    if contains_personal_pronoun?(last_verse) == false
+        return true
+    end
+
+    if contains_same_pronoun?(chosen_verse, last_verse)
+        return true
+    end
+
+    return false
+end
+
+def contains_same_pronoun?(new_verse, old_verse)
+    old_verse = old_verse.split(' ')
+    new_verse = new_verse.split(' ')
+
+    old_pronouns = old_verse & PRONOUNS
+    new_pronouns = new_verse & PRONOUNS
+
+    result = (old_pronouns & new_pronouns).length
+
+    return result > 0 ? true : false
+end
+
+def contains_personal_pronoun?(verse)
+    result = nil
+
+    verse = verse.split(' ')
+
+    # intersection between array with all verse words and all pronouns
+    result = (verse & PRONOUNS).length
+
+    return result > 0 ? true : false
+end
+
 
 def get_verse_data
     data_dir = './data/parsed'
